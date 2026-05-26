9386fa833ce20f67359930a7e07e2557814f5d4b
João Pedro <irus0ubusiness@gmail.com>
2026-05-26 13:00:54 +0100

    add single event route, separate routes and handlers into their own files

--- main.go
+++ main.go
@@ -1,47 +1,15 @@
 package main
 
 import (
-	"net/http"
+	"github.com/gin-gonic/gin"
 
 	"example.com/event_booking_api/db"
-	"example.com/event_booking_api/models"
-	"github.com/gin-gonic/gin"
+	"example.com/event_booking_api/routes"
 )
 
 func main() {
 	db.InitDB()
 	server := gin.Default()
-
-	server.GET("/events", getEvents)
-	server.POST("/events", createEvent)
-
+	routes.RegisterRoutes(server)
 	server.Run(":8080")
-}
-
-func getEvents(c *gin.Context) {
-	events, err := models.GetAllEvents()
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"message": "could not fetch events."})
-		return
-	}
-	c.JSON(http.StatusOK, events)
-}
-
-func createEvent(c *gin.Context) {
-	var event models.Event
-	err := c.ShouldBindJSON(&event)
-	if err != nil {
-		c.JSON(http.StatusBadRequest, gin.H{"message": "could not parse request data"})
-		return
-	}
-
-	event.ID = 1
-	event.UserID = 10
-	err = event.Save()
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"message": "could not create events."})
-		return
-	}
-
-	c.JSON(http.StatusCreated, gin.H{"message": "event created", "event": event})
 }
