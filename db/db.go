4cb50dab867a3f25ffe1ca8a4626eb61884348cc
João Pedro <irus0ubusiness@gmail.com>
2026-05-26 13:25:58 +0100

    fix db typo

--- db/db.go
+++ db/db.go
@@ -10,7 +10,7 @@ var DB *sql.DB
 
 func InitDB() {
 	var err error
-	DB, err = sql.Open("sqlite3", "api.go")
+	DB, err = sql.Open("sqlite3", "api.db")
 	if err != nil {
 		panic("Could not connect to db.")
 	}
